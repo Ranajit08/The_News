3ee142bbe06a8a5efc601dcc3b869a28aa539139
Ranajit Mitra <creativeranajit12@gmail.com>
2025-12-08 17:03:21 +0530

    backend and frontend completed

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from os import name
-from flask import Flask, render_template # type: ignore
-from helpers import get_news
+from flask import Flask, render_template, request # type: ignore
+from helpers import NEWS
 
+d = NEWS()
 app = Flask(__name__)
 app.secret_key = "TheNews"
 
-@app.route("/")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    response = get_news()
-    return render_template("index.html", json = response)
+    if request.method == "GET":
+        response = d.get_news()
+        return render_template("index.html", json = response)
+    else:
+        response = d.news_q(request.form.get("q")) 
+        return render_template("index.html", json = response)
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+     
 if __name__=="__main__":
     app.run(debug=True)
